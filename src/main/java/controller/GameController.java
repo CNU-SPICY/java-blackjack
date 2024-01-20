eec0b919fb4159fa19d94788fc0410be3ce32119
sans29374 <sansu7904@gmail.com>
2024-01-21 00:12:09 +0900

    Controller 수정: GameController 함수 분리

--- src/main/java/controller/GameController.java
+++ src/main/java/controller/GameController.java
@@ -9,39 +9,29 @@ import src.main.java.view.OutputView;
 public class GameController {
     public void run() {
         final var playersNames = InputView.getPlayersNames();
-
         final var players = new Players(playersNames);
         final var dealer = new Dealer();
 
+        startGame(players, dealer);
+        playPlayersRounds(players);
+        playDealerTurn(players, dealer);
+        determineWinners(players, dealer);
+        displayResults(players, dealer);
+    }
+
+    private void startGame(Players players, Dealer dealer) {
         players.startGame(dealer);
         OutputView.displayInitialHands(players.getPlayersHands(), dealer.getFaceUpCard());
+    }
 
+    private void playPlayersRounds(Players players) {
         for (Player player : players.getPlayers()) {
             playPlayersRound(player, players);
-        }
-
-        players.playDealerTurn(dealer);
-        if (dealer.isHasReceive() == true) {
-            OutputView.displayDealerReceivedCard();
-        }
-
-        OutputView.displayDealerResult(dealer.handToString(), dealer.calculateScore());
-        for (Player player : players.getPlayers()) {
-            OutputView.displayPlayerResult(player.getName(), player.handToString(), player.calculateScore());
-        }
-
-        for (Player player : players.getPlayers()) {
-            players.determineWinners(player, dealer);
-        }
-
-        OutputView.displayDealerWins(dealer.getWins(), dealer.getLosses(), dealer.getDraws());
-        for (Player player : players.getPlayers()) {
-            OutputView.displayPlayerWins(player.getName(), player.getWins(), player.getLosses(), player.getDraws());
         }
     }
 
     private void playPlayersRound(Player player, Players players) {
-        while(shouldContinue(player)) {
+        while (shouldContinue(player)) {
             playSingleTurn(player, players);
         }
     }
@@ -53,5 +43,33 @@ public class GameController {
     private void playSingleTurn(Player player, Players players) {
         players.playPlayerTurn(player);
         OutputView.displayPlayerHand(player.getName(), player.handToString());
+    }
+
+    private void playDealerTurn(Players players, Dealer dealer) {
+        players.playDealerTurn(dealer);
+        if (dealer.isHasReceive()) {
+            OutputView.displayDealerReceivedCard();
+        }
+    }
+
+    private void displayResults(Players players, Dealer dealer) {
+        OutputView.displayDealerResult(dealer.handToString(), dealer.calculateScore());
+        for (Player player : players.getPlayers()) {
+            OutputView.displayPlayerResult(player.getName(), player.handToString(), player.calculateScore());
+        }
+        displayFinalWins(players, dealer);
+    }
+
+    private void determineWinners(Players players, Dealer dealer) {
+        for (Player player : players.getPlayers()) {
+            players.determineWinners(player, dealer);
+        }
+    }
+
+    private void displayFinalWins(Players players, Dealer dealer) {
+        OutputView.displayDealerWins(dealer.getWins(), dealer.getLosses(), dealer.getDraws());
+        for (Player player : players.getPlayers()) {
+            OutputView.displayPlayerWins(player.getName(), player.getWins(), player.getLosses(), player.getDraws());
+        }
     }
 }
